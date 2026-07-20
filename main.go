adec98e34183d61a009dd1e84bcde74fdf1445de
Mohan_Amuthan <mohanamuthan@gmail.com>
2026-07-20 23:44:03 +0530

    Priority Locks

--- main.go
+++ main.go
@@ -3,52 +3,31 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-var mutexA sync.Mutex
-var mutexB sync.Mutex
+var (
+	count int
+	lock  sync.Mutex
+)
 
-func resource1(name string) {
-	mutexA.Lock()
-	defer mutexA.Unlock()
-	fmt.Println(name, " has mutex A")
-	fmt.Println(name, " trying to acquire mutex B")
-	if !mutexB.TryLock() {
-		fmt.Println("Cannot Aquire mutex B, Calling off")
-		for range 100 {
-
-		}
-		resource1(name)
-	} else {
-		defer mutexB.Unlock()
-		fmt.Println(name, " acquired mutex B")
-		fmt.Println("working")
-	}
+func highPriority() {
+	lock.Lock()
+	time.Sleep(200 * time.Millisecond)
+	defer lock.Unlock()
+	count = +10
 }
 
-func resource2(name string) {
-	mutexB.Lock()
-	defer mutexB.Unlock()
-	fmt.Println(name, " has mutex B")
-	fmt.Println(name, " trying to acquire mutex A")
-	if !mutexA.TryLock() {
-		fmt.Println("Cannot Aquire mutex A, Calling off")
-		for range 100 {
-
-		}
-		resource2(name)
-	} else {
-		defer mutexA.Unlock()
-		fmt.Println(name, " acquired mutex A")
-		fmt.Println("working")
-	}
-
+func lowPriority() {
+	lock.Lock()
+	defer lock.Unlock()
+	count = +1
 }
-
 func main() {
-	go resource1("Routine A")
-
-	go resource2("Routine B")
-	fmt.Println("Waiting")
-	select {}
+	for range 100 {
+		go highPriority()
+		go lowPriority()
+	}
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println(count)
 }
