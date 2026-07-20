e83495614581f793b1cf819386904c3f4d09d594
Mohan_Amuthan <mohanamuthan@gmail.com>
2026-07-20 16:51:10 +0530

    Channel Lesson in go - For range loop

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+//Branch - Slave0.1
+
+func Square(sq chan int, number int) {
+	digit := make(chan int)
+	go Numbergeneration(number, digit)
+	sum := 0
+	for value := range digit {
+		sum = sum + value*value
+	}
+	sq <- sum
+}
+
+func Cube(cb chan int, number int) {
+	digit := make(chan int)
+	go Numbergeneration(number, digit)
+	sum := 0
+	for value := range digit {
+		sum = sum + value*value*value
+	}
+	cb <- sum
+}
+
+func Numbergeneration(number int, digit chan int) {
+	for number != 0 {
+		value := number % 10
+		digit <- value
+		number = number / 10
+	}
+	close(digit)
+}
+
+var square chan int
+var cube chan int
 
 func main() {
-	fmt.Println("Hello, World")
+	threedigit := rand.IntN(999)
+	fmt.Println("Generate number ", threedigit)
+	square = make(chan int)
+	cube = make(chan int)
+	go Square(square, threedigit)
+	go Cube(cube, threedigit)
+	fmt.Println(<-square, <-cube)
+
 }
