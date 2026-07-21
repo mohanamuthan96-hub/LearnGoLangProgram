5de803b8922bf499ce501d75e14b48bfd60fcb83
Mohan_Amuthan <mohanamuthan@gmail.com>
2026-07-21 15:42:15 +0530

    Go installation and work it works in different ways to compile it

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # LearnGoLangProgram
-We are going have a handson experience in golang
+Installation:
+
+version:
+  # go version
+
+Go working
+ 1. # go install
+ // Compile and install the copy of binary(named as module name) to the go/bin/. directory
+   # binaryfilename
+// To run the binary executable, located in the GOBIN path
+
+ 2. # go build
+ //compile and install the copy in the working directory(go.mod location), we can change the path  
+
+ 3. # go run {main.go}
+ // compile and install in a temporary location, go run --work main.go
--- main.go
+++ main.go
@@ -2,55 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
-type Change struct {
-	mux   sync.RWMutex
-	count int
-}
-
-func (mu Change) Get() int {
-	mu.mux.RLock()
-	defer mu.mux.RUnlock()
-	return mu.count
-}
-
-func (mu *Change) Set() {
-	mu.mux.Lock()
-	defer mu.mux.Unlock()
-	(*mu).count++
-}
-func result(i int, wg *sync.WaitGroup) {
-	fmt.Println("Goroutine ", i)
-	defer wg.Done()
-	fmt.Println("Goroutine ", i, "Executed")
-}
 func main() {
-	wg := &sync.WaitGroup{}
-	count := &Change{
-		count: 0,
-	}
-	wg.Add(10)
-	for i := 0; i < 10; i++ {
-		go func() {
-			for j := 0; j < 100; j++ {
-				count.Set()
-			}
-			wg.Done()
-		}()
-
-	}
-	wg.Wait()
-	wg.Add(10)
-	for i := 0; i < 10; i++ {
-		go func() {
-			fmt.Println("Loop of", i, " ", count.Get())
-			time.Sleep(2 * time.Millisecond)
-			wg.Done()
-		}()
-
-	}
-	wg.Wait()
+	fmt.Println("Hello, World")
 }
