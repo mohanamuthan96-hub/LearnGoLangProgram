81efc177e9f0e1f7ffb3f5e9b163ce7705d8993f
Mohan_Amuthan <mohanamuthan@gmail.com>
2026-07-21 13:06:51 +0530

    RW Mutex - Convienent

--- main.go
+++ main.go
@@ -3,20 +3,54 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
+type Change struct {
+	mux   sync.RWMutex
+	count int
+}
+
+func (mu Change) Get() int {
+	mu.mux.RLock()
+	defer mu.mux.RUnlock()
+	return mu.count
+}
+
+func (mu *Change) Set() {
+	mu.mux.Lock()
+	defer mu.mux.Unlock()
+	(*mu).count++
+}
 func result(i int, wg *sync.WaitGroup) {
 	fmt.Println("Goroutine ", i)
 	defer wg.Done()
 	fmt.Println("Goroutine ", i, "Executed")
 }
 func main() {
-	var wg sync.WaitGroup
+	wg := &sync.WaitGroup{}
+	count := &Change{
+		count: 0,
+	}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				count.Set()
+			}
+			wg.Done()
+		}()
 
-	for i := 1; i <= 25; i++ {
-		wg.Add(1)
-		go result(i, &wg)
 	}
 	wg.Wait()
-	fmt.Println("Gorotine finished")
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			fmt.Println("Loop of", i, " ", count.Get())
+			time.Sleep(2 * time.Millisecond)
+			wg.Done()
+		}()
+
+	}
+	wg.Wait()
 }
