8f34d342d579e78f51ad59cb80692c02491ee599
Mohan_Amuthan <mohanamuthan@gmail.com>
2026-07-22 10:39:50 +0530

    Concrete and value type of interface

--- main.go
+++ main.go
@@ -2,59 +2,24 @@ package main
 
 import "fmt"
 
-type Permanent struct {
-	name   string
-	id     int
-	salary int
-	pf     int
+type Person struct {
+	name  string
+	place string
 }
 
-type Contract struct {
-	name   string
-	id     int
-	salary int
+type Worker interface {
+	work() string
 }
 
-type Freelancer struct {
-	name     string
-	id       int
-	noofhour int
-	noofdays int
+func (per Person) work() (name string) {
+	fmt.Printf("The conrete type of interface %T and value id %v", per, per)
+	return per.name
 }
 
-type Salarycalculator interface {
-	Calculatesalary() int
-}
-
-func (per Permanent) Calculatesalary() int {
-	sum := per.salary + per.pf
-	return sum
-}
-
-func (con Contract) Calculatesalary() int {
-	sum := con.salary
-	return sum
-}
-
-func (fre Freelancer) Calculatesalary() int {
-	sum := fre.noofdays + fre.noofhour
-	return sum
-}
-func Calculatingsalary(salaries []Salarycalculator) int {
-	sum := 0
-	for _, value := range salaries {
-		sum = sum + value.Calculatesalary()
-	}
-	return sum
-}
 func main() {
-	per1 := Permanent{name: "Mohan", id: 1, salary: 10000, pf: 10000}
-	per2 := Permanent{name: "Ram", id: 2, salary: 12000, pf: 10000}
-	con1 := Contract{name: "Arun", id: 01, salary: 10000}
-	free1 := Freelancer{name: "Kajul", id: 001, noofhour: 3, noofdays: 4}
-	var employees []Salarycalculator
-	employees = []Salarycalculator{per1, per2, con1, free1}
-	result := Calculatingsalary(employees)
-	fmt.Println(result)
-
+	per := Person{
+		name: "Mohan", place: "Vellore",
+	}
+	var worker Worker = per
+	worker.work()
 }
