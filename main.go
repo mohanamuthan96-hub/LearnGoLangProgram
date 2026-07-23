dee3ca561bafeccdaf82395653cff8aad7160d0b
Mohan_Amuthan <mohanamuthan96@gmail.com>
2026-07-23 18:59:16 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,34 +4,8 @@ import (
 	"fmt"
 )
 
-type Person struct {
-	name string
-}
 
-type Worker interface {
-	describe()
-}
-
-func (Per Person) describe() {
-	fmt.Println("Name ", Per.name)
-}
-
-//Type Assertion
-func describe(types any) {
-	switch v := types.(type) {
-	case int:
-		fmt.Printf("I am a int and my value is %d\n", types.(int))
-	case string:
-		fmt.Printf("I am a string and my value is %s\n", types.(string))
-	case Worker:
-		v.describe()
-	default:
-		fmt.Printf("Unknowntype")
-	}
-}
 
 func main() {
-	describe("Naveen")
-	describe(100)
-	describe(Person{name: "Aravind"})
+	fmt.Prinln("Hello World")
 }
