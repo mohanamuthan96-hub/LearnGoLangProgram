e1ac97fb0133d076f4517958779839975a3ea389
Mohan_Amuthan <mohanamuthan96@gmail.com>
2026-07-25 08:51:20 +0530

    Add variable declarations and print statement in main
    Refactor main function to include variable declarations and initialization.

--- main.go
+++ main.go
@@ -3,5 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World")
+package main
+
+import "fmt"
+
+var firstname string = "MOhan" // Declaration with initilization with type
+var lastname string            //Declaration
+
+var age = 29 // Declaration with initilization without type
+
+// variable literals
+var (
+	emplcode, empdid int
+	empadd           string = "chennai"
+	empref                  = "EMIS"
+)
+
+func main() {
+	var _firstname string = "MOhan" // Declaration with initilization with type
+	var _lastname string            //Declaration
+
+	var _age = 29 // Declaration with initilization without type
+
+	//variable literals
+	var (
+		_emplcode, _empdid int
+		_empadd            string = "chennai"
+		_empref                   = "EMIS"
+	)
+	fathername := "Nataraj"
+	fmt.Println(_firstname, _lastname, _age, _emplcode, _empdid, _empadd, _empref, fathername)
+}
+
 }
